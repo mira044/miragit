c6cf66153298ff5b4b9073e47f0b5b9769280806
mira <miraallaoua2023@gmail.com>
2025-04-07 11:17:56 -0400

    ajout un text

--- main.cs
+++ main.cs
@@ -5,5 +5,6 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, GitHub! Je m'appelle MIRA allaoua.");
+        Console.WriteLine("Je suis un développeur passionné par la technologie et l'innovation.");
     }
 }
